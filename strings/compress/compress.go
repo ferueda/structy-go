7cdd94512f971247ccf98966e8414b44aaaef711
ferueda <fe.rueda@outlook.com>
2022-05-21 09:56:14 -0700

    add compress problem and solution

---
+++ strings/compress/compress.go
@@ -0,0 +1,28 @@
+package compress
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Compress(s string) string {
+	s += "!"
+	var r []string
+	i, j := 0, 0
+
+	for j < len(s) {
+		if s[j] == s[i] {
+			j += 1
+		} else {
+			if j-i == 1 {
+				r = append(r, string(s[i]))
+			} else {
+				r = append(r, strconv.Itoa(j-i), string(s[i]))
+			}
+
+			i = j
+		}
+	}
+
+	return strings.Join(r, "")
+}
---
+++ strings/compress/compress_test.go
@@ -0,0 +1,26 @@
+package compress
+
+import "testing"
+
+var testTable = []struct {
+	name     string
+	input    string
+	expected string
+}{
+	{"test_00", "ccaaatsss", "2c3at3s"},
+	{"test_01", "ssssbbz", "4s2bz"},
+	{"test_02", "ppoppppp", "2po5p"},
+	{"test_03", "nnneeeeeeeeeeeezz", "3n12e2z"},
+	{"test_04", "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy", "127y"},
+}
+
+func TestCompress(t *testing.T) {
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			result := Compress(test.input)
+			if result != test.expected {
+				t.Errorf("For input '%s', expected result '%s' but got '%s'", test.input, test.expected, result)
+			}
+		})
+	}
+}
