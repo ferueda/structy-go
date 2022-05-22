f65e1203a54255c7cd95261b824428e5b8864429
ferueda <fe.rueda@outlook.com>
2022-05-22 11:58:54 -0700

    add recursive solution

--- linkedlist/linkedListValues/linkedListValues.go
+++ linkedlist/linkedListValues/linkedListValues.go
@@ -4,12 +4,29 @@ import (
 	"github.com/ferueda/structy-go/linkedlist"
 )
 
+// iterative
+
+// func linkedListValues[T any](node *linkedlist.Node[T]) []T {
+// 	r := []T{}
+
+// 	for n := node; n != nil; n = n.Next {
+// 		r = append(r, n.Val)
+// 	}
+
+// 	return r
+// }
+
+// recursive
+
 func linkedListValues[T any](node *linkedlist.Node[T]) []T {
-	r := []T{}
+	values := []T{}
+	return _linkedListValues(node, values)
+}
 
-	for n := node; n != nil; n = n.Next {
-		r = append(r, n.Val)
+func _linkedListValues[T any](node *linkedlist.Node[T], values []T) []T {
+	if node == nil {
+		return values
 	}
-
-	return r
+	values = append(values, node.Val)
+	return _linkedListValues(node.Next, values)
 }
