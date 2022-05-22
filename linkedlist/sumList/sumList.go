687b636c74fb9b9dda07cb785eb3a292c23a9d30
ferueda <fe.rueda@outlook.com>
2022-05-22 11:54:41 -0700

    add recurisve solution

--- linkedlist/sumList/sumList.go
+++ linkedlist/sumList/sumList.go
@@ -4,12 +4,28 @@ import (
 	"github.com/ferueda/structy-go/linkedlist"
 )
 
-func sumList(node *linkedlist.Node[int]) int {
-	s := 0
+// iterative
 
-	for n := node; n != nil; n = n.Next {
-		s += n.Val
+// func sumList(node *linkedlist.Node[int]) int {
+// 	s := 0
+
+// 	for n := node; n != nil; n = n.Next {
+// 		s += n.Val
+// 	}
+
+// 	return s
+// }
+
+// recursive
+
+func sumList(node *linkedlist.Node[int]) int {
+	return _sumList(node, 0)
+}
+
+func _sumList(node *linkedlist.Node[int], sum int) int {
+	if node == nil {
+		return sum
 	}
 
-	return s
+	return _sumList(node.Next, sum+node.Val)
 }
