9cf507e2d201735ca8e36ca80b10819300ff965f
ferueda <fe.rueda@outlook.com>
2022-05-22 11:52:04 -0700

    add recursive solution

--- linkedlist/findValue/findValue.go
+++ linkedlist/findValue/findValue.go
@@ -4,11 +4,27 @@ import (
 	"github.com/ferueda/structy-go/linkedlist"
 )
 
+// iterative
+
+// func findValue[T comparable](node *linkedlist.Node[T], target T) bool {
+// 	for n := node; n != nil; n = n.Next {
+// 		if n.Val == target {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// recursive
+
 func findValue[T comparable](node *linkedlist.Node[T], target T) bool {
-	for n := node; n != nil; n = n.Next {
-		if n.Val == target {
-			return true
-		}
+	if node == nil {
+		return false
 	}
-	return false
+
+	if node.Val == target {
+		return true
+	}
+
+	return findValue(node.Next, target)
 }
