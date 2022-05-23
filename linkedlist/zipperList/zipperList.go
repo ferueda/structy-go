d73e66ec8e890d2207c98745ee08adfb3cbb33fb
ferueda <fe.rueda@outlook.com>
2022-05-22 20:37:28 -0700

    add recursive solution

--- linkedlist/zipperList/zipperList.go
+++ linkedlist/zipperList/zipperList.go
@@ -4,29 +4,51 @@ import (
 	"github.com/ferueda/structy-go/linkedlist"
 )
 
+// iterative
+
+// func zipperList[T any](node1, node2 *linkedlist.Node[T]) *linkedlist.Node[T] {
+// 	tail, curr1, curr2 := node1, node1.Next, node2
+// 	isEven := true
+
+// 	for curr1 != nil && curr2 != nil {
+// 		if isEven {
+// 			tail.Next = curr2
+// 			curr2 = curr2.Next
+// 		} else {
+// 			tail.Next = curr1
+// 			curr1 = curr1.Next
+// 		}
+
+// 		tail = tail.Next
+// 		isEven = !isEven
+// 	}
+
+// 	if curr1 != nil {
+// 		tail.Next = curr1
+// 	}
+// 	if curr2 != nil {
+// 		tail.Next = curr2
+// 	}
+
+// 	return node1
+// }
+
+// recursive
+
 func zipperList[T any](node1, node2 *linkedlist.Node[T]) *linkedlist.Node[T] {
-	tail, curr1, curr2 := node1, node1.Next, node2
-	isEven := true
-
-	for curr1 != nil && curr2 != nil {
-		if isEven {
-			tail.Next = curr2
-			curr2 = curr2.Next
-		} else {
-			tail.Next = curr1
-			curr1 = curr1.Next
-		}
-
-		tail = tail.Next
-		isEven = !isEven
+	if node1 == nil && node2 == nil {
+		return nil
+	}
+	if node1 == nil {
+		return node2
+	}
+	if node2 == nil {
+		return node1
 	}
 
-	if curr1 != nil {
-		tail.Next = curr1
-	}
-	if curr2 != nil {
-		tail.Next = curr2
-	}
+	next := node1.Next
+	node1.Next = node2
+	node2.Next = zipperList(next, node2.Next)
 
 	return node1
 }
