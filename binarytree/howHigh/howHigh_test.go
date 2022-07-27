12893c638fa9ab5d893bfe2954c9a2cf6336d2bc
ferueda <fe.rueda@outlook.com>
2022-07-26 23:18:28 -0700

    create how high test suite

---
+++ binarytree/howHigh/howHigh_test.go
@@ -0,0 +1,56 @@
+package howHigh
+
+import (
+	"testing"
+
+	"github.com/ferueda/structy-go/binarytree"
+)
+
+var testTable = []struct {
+	name     string
+	input    *binarytree.Node[string]
+	expected int
+}{
+	{"test_00",
+		&binarytree.Node[string]{Val: "a",
+			Left: &binarytree.Node[string]{Val: "b",
+				Left:  &binarytree.Node[string]{Val: "d"},
+				Right: &binarytree.Node[string]{Val: "e"}},
+			Right: &binarytree.Node[string]{Val: "c",
+				Right: &binarytree.Node[string]{Val: "f"}}},
+		2},
+
+	{"test_01",
+		&binarytree.Node[string]{Val: "a",
+			Left: &binarytree.Node[string]{Val: "b",
+				Left: &binarytree.Node[string]{Val: "d"},
+				Right: &binarytree.Node[string]{Val: "e",
+					Left: &binarytree.Node[string]{Val: "g"}}},
+			Right: &binarytree.Node[string]{Val: "c",
+				Right: &binarytree.Node[string]{Val: "f"}}},
+		3},
+
+	{"test_02",
+		&binarytree.Node[string]{Val: "a",
+			Right: &binarytree.Node[string]{Val: "c"}},
+		1},
+
+	{"test_03",
+		&binarytree.Node[string]{Val: "a"},
+		0},
+
+	{"test_04",
+		nil,
+		-1},
+}
+
+func TestTreeValueCount(t *testing.T) {
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			result := howHigh(test.input)
+			if result != test.expected {
+				t.Errorf("For inputs '%v', expected result is '%v' but got '%v'", test.input, test.expected, result)
+			}
+		})
+	}
+}
