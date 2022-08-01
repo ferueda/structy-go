629ef8016c53361cfe793a6c374d4f965db5c108
ferueda <fe.rueda@outlook.com>
2022-08-01 12:33:53 -0700

    add level averages problem and solution

---
+++ binarytree/levelAverages/levelAverages.go
@@ -0,0 +1,48 @@
+package levelAverages
+
+import (
+	"github.com/ferueda/structy-go/binarytree"
+	"github.com/ferueda/structy-go/ds"
+	"github.com/ferueda/structy-go/utils"
+)
+
+type queueItem[T utils.Number] struct {
+	node  *binarytree.Node[T]
+	level int
+}
+
+// breadth first (iterative)
+
+func levelAverages[T utils.Number](root *binarytree.Node[T]) []float64 {
+	if root == nil {
+		return []float64{}
+	}
+
+	var levels [][]T
+
+	queue := ds.NewQueue[queueItem[T]]()
+	queue.Enqueue(queueItem[T]{node: root, level: 0})
+
+	for queue.Size() > 0 {
+		item, _ := queue.Dequeue()
+		if item.level == len(levels) {
+			levels = append(levels, []T{item.node.Val})
+		} else {
+			levels[item.level] = append(levels[item.level], item.node.Val)
+		}
+
+		if item.node.Left != nil {
+			queue.Enqueue(queueItem[T]{node: item.node.Left, level: item.level + 1})
+		}
+		if item.node.Right != nil {
+			queue.Enqueue(queueItem[T]{node: item.node.Right, level: item.level + 1})
+		}
+	}
+
+	var avgs []float64
+	for _, level := range levels {
+		avgs = append(avgs, utils.Avg(level))
+	}
+
+	return avgs
+}
