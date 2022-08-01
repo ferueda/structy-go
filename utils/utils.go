47d7476b625c3d426281a204adf34bf635b6e45a
ferueda <fe.rueda@outlook.com>
2022-08-01 12:33:18 -0700

    create Avg util func

--- utils/utils.go
+++ utils/utils.go
@@ -1,15 +1,29 @@
 package utils
 
-func Min(a, b int) int {
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+func Min[T Number](a, b T) T {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-func Max(a, b int) int {
+func Max[T Number](a, b T) T {
 	if a > b {
 		return a
 	}
 	return b
+}
+
+func Avg[T Number](nums []T) float64 {
+	var sum T
+	for _, v := range nums {
+		sum += v
+	}
+	return float64(sum) / float64(len(nums))
 }
