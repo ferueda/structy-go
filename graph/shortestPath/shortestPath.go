c97c44568c0e8b3f4682591a077e020f7d5df852
ferueda <fe.rueda@outlook.com>
2022-08-02 00:01:55 -0700

    add shortest path problem and solution

---
+++ graph/shortestPath/shortestPath.go
@@ -0,0 +1,38 @@
+package shortestPath
+
+import (
+	"github.com/ferueda/structy-go/ds"
+	"github.com/ferueda/structy-go/graph"
+)
+
+type queueItem[T comparable] struct {
+	node     T
+	distance int
+}
+
+// breadth first (iterative)
+
+func shortestPath[T comparable](edges [][]T, src, dst T) int {
+	g := make(graph.Graph[T])
+	graph.BuildGraph(g, edges)
+	visited := make(map[T]bool)
+	queue := ds.NewQueue[queueItem[T]]()
+	queue.Enqueue(queueItem[T]{node: src, distance: 0})
+
+	for queue.Size() > 0 {
+		item, _ := queue.Dequeue()
+		if item.node == dst {
+			return item.distance
+		}
+
+		visited[item.node] = true
+
+		for _, neighbor := range g[item.node] {
+			if _, ok := visited[neighbor]; !ok {
+				queue.Enqueue(queueItem[T]{node: neighbor, distance: item.distance + 1})
+			}
+		}
+	}
+
+	return -1
+}
