17443d3fada0638e09a01215f4e1ecb1df8f17f0
ferueda <fe.rueda@outlook.com>
2022-08-02 00:03:17 -0700

    add connected components count test suite

---
+++ graph/connectedComponentsCount/connectedComponentsCount_test.go
@@ -0,0 +1,78 @@
+package connectedComponentsCount
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/ferueda/structy-go/graph"
+)
+
+var testTable = []struct {
+	name     string
+	input    graph.Graph[int]
+	expected int
+}{
+	{"test_00",
+		graph.Graph[int]{
+			0: {8, 1, 5},
+			1: {0},
+			5: {0, 8},
+			8: {0, 5},
+			2: {3, 4},
+			3: {2, 4},
+			4: {3, 2},
+		},
+		2},
+
+	{"test_01",
+		graph.Graph[int]{
+			1: {2},
+			2: {1, 8},
+			6: {7},
+			9: {8},
+			7: {6, 8},
+			8: {9, 7, 2},
+		},
+		1},
+
+	{"test_02",
+		graph.Graph[int]{
+			3: {},
+			4: {6},
+			6: {4, 5, 7, 8},
+			8: {6},
+			7: {6},
+			5: {6},
+			1: {2},
+			2: {1},
+		},
+		3},
+
+	{"test_03",
+		graph.Graph[int]{},
+		0},
+
+	{"test_04",
+		graph.Graph[int]{
+			0: {4, 7},
+			1: {},
+			2: {},
+			3: {6},
+			4: {0},
+			6: {3},
+			7: {0},
+			8: {},
+		},
+		5},
+}
+
+func TestConnectedComponentsCount(t *testing.T) {
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			result := connectedComponentsCount(test.input)
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("For input '%v', expected result is '%v' but got '%v'", test.input, test.expected, result)
+			}
+		})
+	}
+}
