4202113fcd191b31768df267aed9ee0ad6a504aa
ferueda <fe.rueda@outlook.com>
2022-08-01 17:42:29 -0700

    add BuildGraph graph util

--- graph/graph.go
+++ graph/graph.go
@@ -1,3 +1,11 @@
 package graph
 
 type Graph[T comparable] map[T][]T
+
+func BuildGraph[T comparable](graph Graph[T], edges [][]T) Graph[T] {
+	for _, edge := range edges {
+		graph[edge[0]] = append(graph[edge[0]], edge[1])
+		graph[edge[1]] = append(graph[edge[1]], edge[0])
+	}
+	return graph
+}
