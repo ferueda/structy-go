76d13440d6159b49649d3fc5f5a6bd38e982b87a
ferueda <fe.rueda@outlook.com>
2022-08-03 16:04:39 -0700

    add island count test suite

---
+++ graph/islandCount/islandCount_test.go
@@ -0,0 +1,60 @@
+package islandCount
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testTable = []struct {
+	name     string
+	input    [][]string
+	expected int
+}{
+	{"test_00",
+		[][]string{
+			{"W", "L", "W", "W", "W"},
+			{"W", "L", "W", "W", "W"},
+			{"W", "W", "W", "L", "W"},
+			{"W", "W", "L", "L", "W"},
+			{"L", "W", "W", "L", "L"},
+			{"L", "L", "W", "W", "W"},
+		},
+		3},
+
+	{"test_01",
+		[][]string{
+			{"L", "W", "W", "L", "W"},
+			{"L", "W", "W", "L", "L"},
+			{"W", "L", "W", "L", "W"},
+			{"W", "W", "W", "W", "W"},
+			{"W", "W", "L", "L", "L"},
+		},
+		4},
+
+	{"test_02",
+		[][]string{
+			{"L", "L", "L"},
+			{"L", "L", "L"},
+			{"L", "L", "L"},
+		},
+		1},
+
+	{"test_03",
+		[][]string{
+			{"W", "W"},
+			{"W", "W"},
+			{"W", "W"},
+		},
+		0},
+}
+
+func TestIslandCount(t *testing.T) {
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			result := islandCount(test.input)
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("For input '%v', expected result is '%v' but got '%v'", test.input, test.expected, result)
+			}
+		})
+	}
+}
