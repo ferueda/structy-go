6ee0b8998eb16e3661a000d2815746d015501ef6
ferueda <fe.rueda@outlook.com>
2022-08-03 17:45:29 -0700

    add minimum island test suite

---
+++ graph/minimumIsland/minimumIsland_test.go
@@ -0,0 +1,61 @@
+package minimumIsland
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testTable = []struct {
+	name     string
+	input    [][]string
+	expected int
+}{
+	{"test_00",
+		[][]string{
+			{"W", "L", "W", "W", "W"},
+			{"W", "L", "W", "W", "W"},
+			{"W", "W", "W", "L", "W"},
+			{"W", "W", "L", "L", "W"},
+			{"L", "W", "W", "L", "L"},
+			{"L", "L", "W", "W", "W"},
+		},
+		2},
+
+	{"test_01",
+		[][]string{
+			{"L", "W", "W", "L", "W"},
+			{"L", "W", "W", "L", "L"},
+			{"W", "L", "W", "L", "W"},
+			{"W", "W", "W", "W", "W"},
+			{"W", "W", "L", "L", "L"},
+		},
+		1},
+
+	{"test_02",
+		[][]string{
+			{"L", "L", "L"},
+			{"L", "L", "L"},
+			{"L", "L", "L"},
+		},
+		9},
+
+	{"test_03",
+		[][]string{
+			{"W", "W"},
+			{"L", "L"},
+			{"W", "W"},
+			{"W", "L"},
+		},
+		1},
+}
+
+func TestMinimumIsland(t *testing.T) {
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			result := minimumIsland(test.input)
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("For input '%v', expected result is '%v' but got '%v'", test.input, test.expected, result)
+			}
+		})
+	}
+}
